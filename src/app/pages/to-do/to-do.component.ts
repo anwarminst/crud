8966e57985d964a3c6538d8021437a5c36ec37b2
Md Anwar <anwar.minst1@gmail.com>
2024-10-06 22:27:43 +0530

    added reactive

--- src/app/pages/to-do/to-do.component.ts
+++ src/app/pages/to-do/to-do.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgZorroAntdModule } from '../../../ng-zorro-antd.module';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [NgZorroAntdModule, CommonModule, FormsModule],
+  imports: [NgZorroAntdModule, CommonModule, FormsModule, ReactiveFormsModule,],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.less'
 })
@@ -16,7 +17,13 @@ export class ToDoComponent implements OnInit {
   public title: string = '';
   public description: string = '';
 
+  public todoForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+  });
+
   constructor() {
+
     if (localStorage.getItem('todos')) {
       this.todos = JSON.parse(localStorage.getItem('todos')!);
     }
@@ -32,13 +39,27 @@ export class ToDoComponent implements OnInit {
     this.isToDoAddVisible = false;
   }
   toDoAddhandleOk(): void {
-    if (this.title && this.description) {
-      this.todos.push({ title: this.title, description: this.description });
-      localStorage.setItem('todos', JSON.stringify(this.todos));
+
+    if (this.todoForm.valid) {
+      const { title, description } = this.todoForm.value;
+      if (title && description) {
+        this.todos.push({ title, description });
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+      }
     }
     this.isToDoAddVisible = false;
-    this.title = '';
-    this.description = '';
   }
 
 }
+
+//first import
+//FormsModule, ReactiveFormsModule,
+
+//import FormControl and create input FormControl
+//titleControl = new FormControl('');
+
+//regist in html
+//<input nz - input placeholder = "Please enter title"[formControl] = "titleControl" type = "text" />
+
+//get input value
+//console.log(this.titleControl.value);
